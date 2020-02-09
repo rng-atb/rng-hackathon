1c465cb41db7a3cfb96aa1beea1fac228b541fc6
Elis Frroku <elis.frroku@ucalgary.ca>
2020-02-08 19:54:38 -0700

    Finished implementation of getting all accounts for Flask REST API; getting all transactions by account; and populating data through the JSON files

--- populate/lib/populate_db.py
+++ populate/lib/populate_db.py
@@ -1,8 +1,15 @@
 """
 * You can use a decorator class to raise Exceptions on bad requests
 
-*Get all accounts
-*Get all transactions by accounts
+*Get all accounts [done]
+*Get all transactions by accounts [done]
+
+p = populate()
+print(p.getAllAccountsAtBank())
+print(p.getAllTransactionsByAccount())
+
+c = create_data()
+print(c.createTransactions())
 
 """
 import json
@@ -23,6 +30,7 @@ class populate:
 
 	def __init__(self, token=None, user_id=None, bank_id=None, account=None):
 		self.__HEADER['Authorization'] = 'DirectLogin token=\"eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.i6uCFJJKYmjJvj6S3XoSGvxPB1TsxtbJyCPxgtB6ZFI\"'
+		self.__HEADER['Content-Type'] = 'application/json'
 
 	def getCurrentUser(self):
 		url = self.__BASE__URL + "users/current"
@@ -54,18 +62,20 @@ class populate:
 		return r.text
 
 	def createTransaction(self, description=None, account=None):
-		self.__ACCOUNT = account if account is not None and self.__ACCOUNT is None else None
+		self.__ACCOUNT = account if self.__ACCOUNT is not None else account
 		amount = random.randint(1, 20)
 		url = self.__BASE__URL + "banks/" + self.__BANK__ID \
 				+ "/accounts/{ACCOUNT_ID}/{VIEW_ID}/transaction-request-types/FREE_FORM/transaction-requests".format(ACCOUNT_ID=self.__ACCOUNT, VIEW_ID=self.__VIEW__ID)
-		data = {"value":{"currency":"CAD","amount":amount},"description":description}
-		r = requests.post(url, data=data,headers=self.__HEADER)
-		print(data)
-		print(url)
-		print(r.status_code)
-#		if (r.status_code is not 200):
-#			raise Exception("Incorrect request!")
-#		return r.text
+		body = {}
+		body["value"]={}
+		body["value"]["currency"]="CAD"
+		body["value"]["amount"]=str(amount)
+		body["description"]=description
+		r = requests.post(url, json=body,headers=self.__HEADER)
+		if (r.status_code is not 201):
+			error = "Incorrect request! ({status_code})"
+			raise Exception(error.format(status_code=r.status_code)
+		return r.text
 
 	def getAllTransactionsByAccount(self, bank_id=None, account=None):
 		self.__ACCOUNT = account if account is not None and self.ACCOUNTS is None else None
@@ -97,19 +107,12 @@ class create_data:
 			self.account_data = json.loads(f.read())
 
 	def createTransactions(self):
-		try:
-			for account in self.account_data:
-				for label in self.label_data:
-					print(label)
-					for merchant in label:
+		labels = self.label_data.keys()
+		accounts = self.account_data.keys()
+		for account in accounts:
+			for label in labels:
+				merchants = self.label_data[label]
+				for merchant in merchants:
+					num = random.randint(0,3)
+					if (num is not 0):
 						self.populate_data.createTransaction(merchant, account)
-		except Exception as e:
-			print(e.args)
-			#print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno)
-#Testing
-#p = populate()
-#print(p.getAllAccountsAtBank())
-#print(p.getAllTransactionsByAccount())
-
-c = create_data()
-print(c.createTransactions())
