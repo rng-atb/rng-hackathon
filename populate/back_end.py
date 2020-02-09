2e557cdda4f681721f7305c98e1c8713549064f8
Alex <ta.alex@outlook.com>
2020-02-08 19:58:10 -0700

    redo the format of json object to return, add names to accounts

--- populate/back_end.py
+++ populate/back_end.py
@@ -1,20 +1,90 @@
 import json
+import sys
+sys.path.insert(1,'lib/')
+from populate_db import populate
+
 
 class Profile:
     __CATEGORIES = None
+    __ACCOUNTSANDNAMES = None
     __PROFILES = []
+    __ACCOUNTLIST = []
     __POPULATE = None
+    __ACCOUNTTONAME = []
     
     def __init__(self, jsonFile):
-        with open(jsonFile) as f:
-            self.__CATEGORIES = json.loads(f)
+        with open("lib/labels.json") as f:
+            self.__CATEGORIES = json.loads(f.read())
+        with open("libs/accounts.json") as f:
+            self.__ACCOUNTSANDNAMES = json.loads(f.read())
+            
         self.__POPULATE = populate()
-        accounts = self.__POPULATE.getAllAccountsAtBank()
-        for account in accounts:
-            if(account['id'].Contains("account_")):
-                self.__PROFILES[account['id']] = None
 
-    def populateProfiles():
+    def getProfile(self):
+        return self.__PROFILES
+
+    def populateProfile(self):
+        accounts = self.__ACCOUNTSANDNAMES.keys()
+        
+        for account in accounts:
+            transactions = self.__POPULATE.getAllTransactionsByAccount(account)
+            name = self.__ACCOUNTSANDNAMES[account]
+            
+            for transaction in transactions:
+                if(transaction['details']['type'] == "FREE_FORM"):
+                    merchant = transaction['details']['description']
+                    label = self.getLabel(merchant)
+                    found = False
+                    for (jsonData in self.__PROFILES):
+                        if(jsonData['source'] = name and jsonData['target'] = label):
+                            jsonData['weight'] += 1
+                            found = True
+                            break
+                    if(found = False):
+                        jsonToAdd['source'] = name
+                        jsonToAdd['target'] = label
+                        jsonToAdd['weight'] = 1
+                        self.__PROFILES.append(jsonToAdd)
+'''
+format:
+[
+  {
+    "source": "Account1",
+    "target": "Learning",
+    "value": 10
+  },
+  {
+    "source": "Account1",
+    "target": "Outdoors",
+    "value": 20
+  },
+  {
+    "source": "Account2",
+    "target": "Gaming",
+    "value": 10
+  },
+  {
+    "source": "Account3",
+    "target": "Learning",
+    "value": 10
+  }
+]
+'''
+                            
+
+    def getMerchant(self, merchant):
+        labels = self.__CATEGORIES.keys()
+        for label in labels:
+            if(self.__CATEGORIES[label].contains(merchant)):
+                return label
+        raise Exception("Cant find label for merchant: " + merchant)
+
+
+profile = Profile()
+profile.populateProfile()
+print(profile.getProfile())
+
+            
         
             
     
